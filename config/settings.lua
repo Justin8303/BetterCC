778e21fb3dc0ccb19164104edc3a68e4982886ad
Justin8303 <32563849+Justin8303@users.noreply.github.com>
2019-06-15 23:44:53 +0200

    Update settings.lua

--- config/settings.lua
+++ config/settings.lua
@@ -1,1 +1,141 @@
+file = fs.open("/root/config/shortcuts.dat","r")
+shortcuts = textutils.unserialize(file.readLine())
+file.close()
+
+beforefsopen = fs.open
+fs.open = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsopen(unpack(input))
+end
+
+beforefslist = fs.list
+fs.list = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefslist(unpack(input))
+end
+
+beforefsexists = fs.exists
+fs.exists = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsexists(unpack(input))
+end
+
+beforefsisDir = fs.isDir
+fs.isDir = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsisDir(unpack(input))
+end
+
+beforefsisReadOnly = fs.isReadOnly
+fs.isReadOnly = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsisReadOnly(unpack(input))
+end
+
+beforegetName = fs.getName
+fs.getName = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforegetName(unpack(input))
+end
+
+beforefsgetSize = fs.getSize
+fs.getSize = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsgetSize(unpack(input))
+end
+
+beforefsgetFreeSpace = fs.getFreeSpace
+fs.getFreeSpace = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsgetFreeSpace(unpack(input))
+end
+
+beforefsmakeDir = fs.makeDir
+fs.makeDir = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsmakeDir(unpack(input))
+end
+
+beforefsmove = fs.move
+fs.move = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	if shortcuts[input[2]] then
+		input[2] = shortcuts[input[2]]
+	end
+	return beforefsmove(unpack(input))
+end
+
+beforefscopy = fs.copy
+fs.copy = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	if shortcuts[input[2]] then
+		input[2] = shortcuts[input[2]]
+	end
+	return beforefscopy(unpack(input))
+end
+
+beforefsdelete = fs.delete
+fs.delete = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforefsdelete(unpack(input))
+end
+
+beforefscombine = fs.combine
+fs.combine = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	if shortcuts[input[2]] then
+		input[2] = shortcuts[input[2]]
+	end
+	return beforefscombine(unpack(input))
+end
+
+beforeloadfile = loadfile
+loadfile = function(...)
+	input = {...}
+	if shortcuts[input[1]] then
+		input[1] = shortcuts[input[1]]
+	end
+	return beforeloadfile(unpack(input))
+end
+--auto added
 shell.setAlias("list","/root/programs/basics/list")
+shell.setAlias("edit","/root/programs/luaide/luaIDE.lua")
