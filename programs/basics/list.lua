ad0754c451452fa13ed608c99a640562f68f3cce
Justin8303 <32563849+Justin8303@users.noreply.github.com>
2019-06-16 00:22:35 +0200

    Update list.lua

--- programs/basics/list.lua
+++ programs/basics/list.lua
@@ -42,48 +42,4 @@ if term.isColour() then
 	textutils.pagedTabulate( colors.blue, tDirs, colors.lightGray, tFiles , colors.green, tLuaFiles , colors.orange, startup)
 else
 	textutils.pagedTabulate( tDirs, tFiles, tLuaFiles )
-endlocal tArgs = { ... }
-
--- Get all the files in the directory
-local sDir = shell.dir()
-if tArgs[1] ~= nil then
-	sDir = shell.resolve( tArgs[1] )
-end
-
--- Sort into dirs/files, and calculate column count
-local tAll = fs.list( sDir )
-local tFiles = {}
-
-local tLuaFiles = {}
-local startup = {}
-
-local tDirs = {}
-
-local bShowHidden = settings.get( "list.show_hidden" )
-for n, sItem in pairs( tAll ) do
-	if bShowHidden or string.sub( sItem, 1, 1 ) ~= "." then
-		local sPath = fs.combine( sDir, sItem )
-		if fs.isDir( sPath ) then
-			table.insert( tDirs, sItem )
-		else
-			if sItem:match("%.%blua$") then
-				table.insert( tLuaFiles, sItem )
-			elseif sItem == "startup" then
-				if fs.exists("startup") then
-					table.insert( startup, sItem )
-				end
-			else
-				table.insert( tFiles, sItem )
-			end
-		end
-	end
-end
-table.sort( tDirs )
-table.sort( tFiles )
-table.sort( tLuaFiles )
-
-if term.isColour() then
-	textutils.pagedTabulate( colors.blue, tDirs, colors.lightGray, tFiles , colors.green, tLuaFiles , colors.orange, startup)
-else
-	textutils.pagedTabulate( tDirs, tFiles, tLuaFiles )
 end
